a5a753f182fcc7cb0e1d741a7addde739486da63
Ishani Karmarkar <ikarmarkar@fb.com>
2018-07-02 11:03:54 -0700

    Showing feed with user images!

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -3,12 +3,18 @@ package com.codepath.apps.restclienttemplate;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
+import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -16,25 +22,53 @@ public class TimelineActivity extends AppCompatActivity {
 
     private TwitterClient client;
     Context context;
+    TweetAdapter tweetAdapter;
+    ArrayList<Tweet> tweets;
+    RecyclerView rvTweets;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
-
-        // Todo -- Why is this weird? Is this correct?
-        context = getBaseContext();
-
+        context = this;
         client = TwitterApplication.getRestClient(context);
+        // find the recycler view
+        rvTweets = (RecyclerView) findViewById(R.id.rvTweet);
+        // init the arrayList data source
+        tweets = new ArrayList<Tweet>();
+        // construct the adapter from this data source
+        tweetAdapter = new TweetAdapter(tweets);
+        // recycler view setup
+        rvTweets.setLayoutManager(new LinearLayoutManager(this));
+        // set the adapter
+        rvTweets.setAdapter(tweetAdapter);
+        // populate timeline
         populateTimeline();
-
     }
 
     private void populateTimeline() {
         client.getHomeTimeline(new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-                Log.d("Twitter Client", response.toString());
+                // Log.d("Twitter Client", response.toString());
+                // iterate through the JSONArray
+                // for each entry, deserialize the JSON object
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        // convert each object into a tweet model
+                        Tweet tweet = Tweet.fromJSON(response.getJSONObject(i));
+                        // add that tweet model to our data source
+                        tweets.add(tweet);
+                        // notify the adapter that we've added an item
+                        tweetAdapter.notifyItemInserted(tweets.size() - 1);
+                    }
+                    catch (JSONException e){
+                        e.printStackTrace();
+                    }
+                }
+                // convert each object into a Tweet model
+                // add that Tweet model to our data source
+                // notify the adapter that we've added an item
             }
 
             @Override
