d268ae2826efe576db588ecedbd572ab4f246f7c
Ishani Karmarkar <ikarmarkar@fb.com>
2018-07-02 15:25:16 -0700

    Compose tweets!

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -4,9 +4,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
@@ -23,6 +26,7 @@ public class ComposeActivity extends AppCompatActivity {
     private TwitterClient client;
     Context context;
     String message;
+    TextView tvCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +35,30 @@ public class ComposeActivity extends AppCompatActivity {
         context = this;
         client = TwitterApplication.getRestClient(context);
 
-        EditText simpleEditText = (EditText) findViewById(R.id.et_simple);
+        final EditText simpleEditText = (EditText) findViewById(R.id.et_simple);
+        tvCount = (TextView) findViewById(R.id.tvCount);
         message = simpleEditText.getText().toString();
+        simpleEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                int length = simpleEditText.length();
+                int remaining = 140 - length;
+                tvCount.setText(String.format("%d out of 140", remaining));
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
         // set the title bar to reflect the purpose of the view
         getSupportActionBar().setTitle("Compose Tweet");
-
     }
 
     public void onSubmit(View v) {
